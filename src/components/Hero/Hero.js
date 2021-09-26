4f54d33c1cfbf030ee2d9859469873f6932155ca
unknown <mobinh264@gmail.com>
2021-09-26 23:46:13 +0600

    full fill work done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import './Hero.css'
 const Hero = () => {
     return (
         <div className="hero-section">
-             <h1 className="heading">Weaddign Ceremony </h1>
+             <h1 className="heading">Wedding Ceremony</h1>
              <p className="para">In this Ceremony,<u> <b>MD: Junayed Hawladar Raj</b></u> And  <u><b>Megla</b></u> is Getting Married And This list Define all the familly Members Whom Can be in invited</p>
              <h2 className="secondary">Our Wedding Budget : 10millon </h2>
         </div>
